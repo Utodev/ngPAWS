ced892f97e3a578f531a2f136c829fd4eaa994e7
Uto <utodev@gmail.com>
2022-08-31 22:35:19 +0200

    Updated build linux script, for error, check, readability and cleaning

--- build_linux.sh
+++ build_linux.sh
@@ -1,74 +1,104 @@
 #!/bin/bash
 
-#Accept parameter -es for Spanish build, English otherwise
-
 COLOR='\033[0;36m'
 NOCOLOR='\033[0m'
 
-
-echo "\n\n${COLOR}--------------------\nngPAWS linux builder\n--------------------${NOCOLOR}";
-echo "${COLOR}*${NOCOLOR} You need Lazarus, git, and gcc installed on your system\n\n";
+printf "\n\n${COLOR}--------------------\nngPAWS linux builder\n--------------------${NOCOLOR}\n\n";
+printf "${COLOR}*${NOCOLOR} You need Lazarus, git, and gcc installed on your system\n\n";
 if [ $(which lazbuild) = "" ]; then
-   echo "lazbuild coudn't be found. Please install it"
+   printf "lazbuild coudn't be found. Please install Lazarus"
 else
    #Start the build
    
    #build ngPAWS
    if [ -d "build" ]; then 
-      echo "${COLOR}*${NOCOLOR} Deleting build directory\n"
+      printf "${COLOR}*${NOCOLOR} Deleting build directory\n"
       rm -Rf build
    fi
-   
-   echo "${COLOR}*${NOCOLOR} Building IDE\n"
+
+   if [ -d "Release-linux-en" ]; then 
+      printf "${COLOR}*${NOCOLOR} Deleting Release-linux-en directory\n"
+      rm -Rf Release-linux-en
+   fi
+   if [ -d "Release-linux-es" ]; then 
+      printf "${COLOR}*${NOCOLOR} Deleting Release-linux-es directory\n"
+      rm -Rf Release-linux-es
+   fi
+
+   if [ -d "Release-linux-es" ]; then 
+      printf "${COLOR}*${NOCOLOR} Deleting Release-linux-es directory\n"
+      rm -Rf Release-linux-es
+   fi
+
+
+   printf "${COLOR}*${NOCOLOR} Building IDE (may take a while)\n"
    mkdir build
    cp -R newIDE build/newIDE
    lazbuild build/newIDE/ngpaws_linux.lpi
+   rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
-   echo "\n${COLOR}*${NOCOLOR} Building npgc compiler\n"
+
+   printf "\n${COLOR}*${NOCOLOR} Building npgc compiler\n"
    cp -R compiler build/compiler/
    make -C build/compiler/
+   rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
 
    #build txtpaws
    if [ -d "txtpaws" ]; then
-      echo "\n${COLOR}*${NOCOLOR} txtpaws project directory exists\n"
+      printf "\n${COLOR}*${NOCOLOR} txtpaws project directory exists\n"
    else
-      echo "\n${COLOR}*${NOCOLOR} Cloning txtpaws\n"
+      printf "\n${COLOR}*${NOCOLOR} Cloning txtpaws\n"
       git clone https://github.com/Baltasarq/txtpaws/
    fi
    
    make -C txtpaws/src/ clean
+   rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
    make -C txtpaws/src/ txtpaws
+   rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
    
-   #prepare the dist files
-   if [ -d "dist" ]; then
-      echo "\n${COLOR}*${NOCOLOR} Deleting dist directory\n"
-      rm -Rf dist
+   #prepare the Release files
+   if [ -d "Release-linux" ]; then
+      printf "\n${COLOR}*${NOCOLOR} Deleting Release directory\n"
+      rm -Rf Release-linux
    fi
-   if [ -d "dist-en" ]; then
-      echo "${COLOR}*${NOCOLOR} Deleting dist-en directory\n"
-      rm -Rf dist-en
+   if [ -d "Release-linux-en" ]; then
+      printf "${COLOR}*${NOCOLOR} Deleting Release-linux-en directory\n"
+      rm -Rf Release-linux-en
    fi
-   if [ -d "dist-es" ]; then
-      echo "${COLOR}*${NOCOLOR} Deleting dist-es directory\n"
-      rm -Rf dist-es
+   if [ -d "Release-linux-es" ]; then
+      printf "${COLOR}*${NOCOLOR} Deleting Release-linux-es directory\n"
+      rm -Rf Release-linux-es
    fi
    
-   echo "${COLOR}*${NOCOLOR} Prepare files for distribution\n"
-   mkdir dist
+   printf "${COLOR}*${NOCOLOR} Prepare files for Release\n"
+   mkdir Release-linux
 
-   mv build/newIDE/ngpaws dist/ngpaws
-   mv build/compiler/ngpc dist/ngpc
-   cp txtpaws/src/txtpaws dist/txtpaws
-   cp -R jsl example-code extra_langs installation_aux_files/* plugins dist/
+   mv build/newIDE/ngpaws Release-linux/ngpaws
+   mv build/compiler/ngpc Release-linux/ngpc
+   cp txtpaws/src/txtpaws Release-linux/txtpaws
+   cp -R jsl example-code extra_langs installation_aux_files/* plugins Release-linux/
    
-   cp -R dist dist-es
-   mv dist dist-en
+   cp -R Release-linux Release-linux-es
+   mv Release-linux Release-linux-en
    
-   echo "${COLOR}*${NOCOLOR} Setting Spanish databse as default for dist-es"
-   mv dist-es/database.start.spanish dist-es/database.start
+   printf "${COLOR}*${NOCOLOR} Setting Spanish database as default for Release-linux-es"
+   mv Release-linux-es/database.start.spanish Release-linux-es/database.start
    
-   echo "${COLOR}*${NOCOLOR} Setting English databse as default for dit-en"
-   mv dist-en/database.start.english dist-en/database.start
+   printf "${COLOR}*${NOCOLOR} Setting English database as default for Release-linux-en"
+   mv Release-linux-en/database.start.english Release-linux-en/database.start
+
+   printf "${COLOR}*${NOCOLOR} Cleaning build folders\n\n"
+   if [ -d "txtpaws" ]; then
+      rm -Rf txtpaws   
+   fi 
+
+   if [ -d "build" ]; then
+      rm -Rf build
+   fi 
    
-   echo "\n${COLOR}--------------------\nDone\n--------------------${NOCOLOR}";
+   printf "${COLOR}* English release is in Release-linux-en folder\n"
+   printf "${COLOR}* Spanish release is in Release-linux-es folder\n"
+
+   printf "\n${COLOR}--------------------\nDone\n--------------------${NOCOLOR}\n\n";
 fi
